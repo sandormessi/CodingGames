7c60f35763c3dcf8cc30c51b12f80cb1a31fcbfd
Sándor Ács <sandormessi@hotmail.com>
2023-06-05 20:21:46 +0200

    ImplementGame class

--- Puzzles/PowerOfThorEpisode1/Implementation/Game/PowerOfThorEpisode1Game.cs
+++ Puzzles/PowerOfThorEpisode1/Implementation/Game/PowerOfThorEpisode1Game.cs
@@ -3,14 +3,21 @@
 using System;
 using PowerOfThor.Core.Abstraction.Game;
 using PowerOfThor.Core.Abstraction.InputReading;
+using PowerOfThor.Core.Abstraction.Logic;
 
 public class PowerOfThorEpisode1Game : IPowerOfThorGame
 {
     private readonly IGameDataReader gameDataReader;
 
-    public PowerOfThorEpisode1Game(IGameDataReader gameDataReader)
+    private readonly IOutputManager outputManager;
+
+    private readonly ICoordinateDirectionCalculator coordinateDirectionCalculator;
+
+    public PowerOfThorEpisode1Game(IGameDataReader gameDataReader, IOutputManager outputManager, ICoordinateDirectionCalculator coordinateDirectionCalculator)
     {
         this.gameDataReader = gameDataReader ?? throw new ArgumentNullException(nameof(gameDataReader));
+        this.outputManager = outputManager ?? throw new ArgumentNullException(nameof(outputManager));
+        this.coordinateDirectionCalculator = coordinateDirectionCalculator ?? throw new ArgumentNullException(nameof(coordinateDirectionCalculator));
     }
 
     public void Execute()
@@ -18,6 +25,12 @@ public class PowerOfThorEpisode1Game : IPowerOfThorGame
         while (true)
         {
             var gameData = gameDataReader.ReadGameData();
+
+            var initialGameData = gameData.InitialGameData;
+
+            var direction = coordinateDirectionCalculator.CalculateDirectionOfCoordinates(initialGameData.ThorPosition, initialGameData.LightPosition);
+
+            outputManager.MoveThor(direction);
         }
     }
 }
