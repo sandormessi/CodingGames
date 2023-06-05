0e313adc0200ab8d66c2380db4a5ec4c377ab19c
Sándor Ács <sandormessi@hotmail.com>
2023-06-05 20:21:26 +0200

    Add CoordinateDirectionCalculator

---
+++ Puzzles/PowerOfThor.Core/Abstraction/Logic/ICoordinateDirectionCalculator.cs
@@ -0,0 +1,8 @@
+﻿namespace PowerOfThor.Core.Abstraction.Logic;
+
+using Data;
+
+public interface ICoordinateDirectionCalculator
+{
+    Directions CalculateDirectionOfCoordinates(Coordinate2D coordinate1, Coordinate2D coordinate2);
+}
