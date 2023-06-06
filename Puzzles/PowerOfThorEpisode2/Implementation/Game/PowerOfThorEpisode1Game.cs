ce01f672409512036638b8ab203277446c75e47f
Sándor Ács <sandormessi@hotmail.com>
2023-06-06 21:00:23 +0200

    Add new puzzle (Power of Thor - Episode 2) project

---
+++ Puzzles/PowerOfThorEpisode2/Implementation/Game/PowerOfThorEpisode1Game.cs
@@ -0,0 +1,40 @@
+﻿using CodingGames.Core.Abstraction;
+using CodingGames.Core.Implementation;
+using PowerOfThor.Core.Abstraction.Data;
+using PowerOfThor.Core.Abstraction.Game;
+using PowerOfThor.Core.Abstraction.InputReading;
+using PowerOfThor.Core.Abstraction.Logic;
+using PowerOfThor.Core.Implementation.Game;
+using PowerOfThor.Core.Implementation.InputReading;
+using PowerOfThor.Core.Implementation.Logic;
+
+namespace PowerOfThorEpisode2.Implementation.Game;
+
+public class PowerOfThorEpisode2Game : IPowerOfThorGame
+{
+    public void Execute()
+    {
+        IInputReader inputReader = new InputReader();
+        IOutputManager outputManager = new OutputManager();
+        IInitialGameDataReader initialGameDataReader = new InitialGameDataReader(inputReader);
+        IGameDataPerRoundReader gameDataPerRoundReader = new GameDataPerRoundReader(inputReader);
+        IActualMovementCalculator actualMovementCalculator = new ActualMovementCalculator();
+
+        IGameDataReader gameDataReader = new GameDataReader(initialGameDataReader, gameDataPerRoundReader);
+        IGameLogicManager gameLogicManager = new GameLogicManager(outputManager, actualMovementCalculator);
+
+        int round = 1;
+
+        GameData gameData = gameDataReader.ReadGameData(round);
+
+        var lastPosition = gameData.InitialGameData.ThorPosition;
+        while (true)
+        {
+            var actualMovement = gameLogicManager.Execute(gameData, lastPosition);
+            lastPosition = actualMovement.ActualPosition;
+            round++;
+            
+            gameData = gameDataReader.ReadGameData(round);
+        }
+    }
+}
