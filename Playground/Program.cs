efbb39a7fffb78f4be825552b2511cc462afe45e
Sándor Ács <sandormessi@hotmail.com>
2023-06-06 20:29:10 +0200

    Add playground console app

---
+++ Playground/Program.cs
@@ -0,0 +1,14 @@
+﻿using PowerOfThorEpisode1.Implementation.Game;
+
+namespace Playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new PowerOfThorEpisode1Game();
+
+            game.Execute();
+        }
+    }
+}
