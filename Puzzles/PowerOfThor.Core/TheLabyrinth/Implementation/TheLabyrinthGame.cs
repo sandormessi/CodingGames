a9555c33df0735cb915eb2aee4c6310e1e0e0cd0
Sándor Ács <sandormessi@hotmail.com>
2023-06-19 22:27:00 +0200

    Game service implementation first implementation

---
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Implementation/TheLabyrinthGame.cs
@@ -0,0 +1,30 @@
+﻿namespace TheLabyrinth;
+
+using CodingGames.Core.Abstraction;
+using CodingGames.Core.Implementation;
+
+using TheLabyrinth.Abstraction;
+
+public class TheLabyrinthGame : ITheLabyrinthGame
+{
+   public void StartGame()
+   {
+      IInputReader inputReader = new InputReader();
+      IGameLogicManager gameLogicManager = new GameLogicManager();
+      IInitialGameInfoReader initialGameInfoReader = new InitialGameInfoReader(inputReader);
+      ILabyrinthCellReader labyrinthCellReader = new LabyrinthCellReader();
+      ILabyrinthReader labyrinthReader = new LabyrinthReader(labyrinthCellReader);
+      IRoundGameInfoReader roundInfoReader = new RoundGameInfoReader(inputReader, labyrinthReader);
+
+      var initialGameInfo = initialGameInfoReader.ReadInitialGameInfo();
+
+      int round = 1;
+      while (true)
+      {
+         var roundGameInfo = roundInfoReader.ReadRoundGameInfo(initialGameInfo, round);
+         gameLogicManager.ExecuteLogic(initialGameInfo, roundGameInfo);
+
+         round++;
+      }
+   }
+}
