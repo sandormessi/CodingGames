249fca9da5fabb8f649edc7f0568767c732b3b3e
Sándor Ács <sandormessi@hotmail.com>
2023-06-19 22:26:36 +0200

    GameLogicManager (not implemented)

---
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/GameLogicManager.cs
@@ -0,0 +1,21 @@
+﻿namespace TheLabyrinth;
+
+using System;
+
+public class GameLogicManager : IGameLogicManager
+{
+   public void ExecuteLogic(InitialGameInfo initialGameInfo, RoundGameInfo roundGameInfo)
+   {
+      if (initialGameInfo == null)
+      {
+         throw new ArgumentNullException(nameof(initialGameInfo));
+      }
+
+      if (roundGameInfo == null)
+      {
+         throw new ArgumentNullException(nameof(roundGameInfo));
+      }
+
+      throw new NotImplementedException();
+   }
+}
