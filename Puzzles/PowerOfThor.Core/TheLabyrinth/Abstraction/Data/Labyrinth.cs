a918ea2b870850d335c14d50d3a1d2f59771ceed
Sándor Ács <sandormessi@hotmail.com>
2023-06-20 22:12:44 +0200

    Add Position check to Labyrinth

--- Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/Data/Labyrinth.cs
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/Data/Labyrinth.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 namespace TheLabyrinth;
 
+using System.Linq;
+
 public abstract class Labyrinth<TCell>
    where TCell : LabyrinthCell
 {
@@ -14,6 +16,16 @@ public abstract class Labyrinth<TCell>
    public TCell this[Position position] => Cells[position.Y][position.X];
 
    public TCell this[int x, int y] => Cells[y][x];
+
+   public bool IsPositionInLabyrinth(Position position)
+   {
+      if (position == null)
+      {
+         throw new ArgumentNullException(nameof(position));
+      }
+
+      return position.X >= 0 && position.X < Cells.First().Count && position.Y >= 0 && position.X < Cells.Count;
+   }
 
    public IReadOnlyList<IReadOnlyList<TCell>> Cells { get; }
 }
