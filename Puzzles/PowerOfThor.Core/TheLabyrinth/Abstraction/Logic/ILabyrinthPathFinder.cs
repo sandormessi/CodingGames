1a01d364df4700fff47af0bf12660763b4fa1495
Sándor Ács <sandormessi@hotmail.com>
2023-06-20 20:10:19 +0200

    Labyrinth path finder interface

---
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/Logic/ILabyrinthPathFinder.cs
@@ -0,0 +1,8 @@
+﻿namespace TheLabyrinth.Abstraction.Logic;
+
+using TheLabyrinth.Abstraction.Data;
+
+public interface ILabyrinthPathFinder
+{
+   LabyrinthPath FindPath(LabyrinthCell startCell, LabyrinthCell targetCell);
+}
