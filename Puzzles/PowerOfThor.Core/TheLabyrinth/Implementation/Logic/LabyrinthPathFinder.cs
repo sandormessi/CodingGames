59c396b57ce95078f4905804b96c4a96452929a5
Sándor Ács <sandormessi@hotmail.com>
2023-06-20 20:11:48 +0200

    LabyrinthPathFinder empty implementation

---
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Implementation/Logic/LabyrinthPathFinder.cs
@@ -0,0 +1,23 @@
+﻿namespace TheLabyrinth.Abstraction.Logic;
+
+using System;
+
+using TheLabyrinth.Abstraction.Data;
+
+public class LabyrinthPathFinder : ILabyrinthPathFinder
+{
+   public LabyrinthPath FindPath(LabyrinthCell startCell, LabyrinthCell targetCell)
+   {
+      if (startCell == null)
+      {
+         throw new ArgumentNullException(nameof(startCell));
+      }
+
+      if (targetCell == null)
+      {
+         throw new ArgumentNullException(nameof(targetCell));
+      }
+
+      throw new NotImplementedException();
+   }
+}
