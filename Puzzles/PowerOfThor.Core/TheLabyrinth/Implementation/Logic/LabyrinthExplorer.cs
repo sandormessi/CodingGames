b7243851df1519fea83e232b542f4751d5cdcf80
Sándor Ács <sandormessi@hotmail.com>
2023-06-20 20:11:29 +0200

    LabyrinthExplorer empty implementation

---
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Implementation/Logic/LabyrinthExplorer.cs
@@ -0,0 +1,30 @@
+﻿namespace TheLabyrinth.Abstraction.Logic;
+
+using System;
+
+using TheLabyrinth.Abstraction.Data;
+
+public class LabyrinthExplorer : ILabyrinthExplorer
+{
+   private readonly IOutputManager outputManager;
+
+   public LabyrinthExplorer(IOutputManager outputManager)
+   {
+      this.outputManager = outputManager ?? throw new ArgumentNullException(nameof(outputManager));
+   }
+
+   public TargetCells? ExploreLabyrinth(Labyrinth labyrinth, InitialGameInfo initialGameInfo)
+   {
+      if (labyrinth == null)
+      {
+         throw new ArgumentNullException(nameof(labyrinth));
+      }
+
+      if (initialGameInfo == null)
+      {
+         throw new ArgumentNullException(nameof(initialGameInfo));
+      }
+
+      throw new NotImplementedException();
+   }
+}
