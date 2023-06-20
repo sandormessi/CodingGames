3bb9800476bbe799fb2617c51ac910ec99047358
Sándor Ács <sandormessi@hotmail.com>
2023-06-20 20:09:00 +0200

    TargetCells data class

---
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/Data/TargetCells.cs
@@ -0,0 +1,19 @@
+﻿namespace TheLabyrinth.Abstraction.Data;
+
+using System;
+
+public class TargetCells
+{
+   public TargetCells(LabyrinthCell actualCell, LabyrinthCell targetCell1, LabyrinthCell targetCell2)
+   {
+      ActualCell = actualCell ?? throw new ArgumentNullException(nameof(actualCell));
+      TargetCell1 = targetCell1 ?? throw new ArgumentNullException(nameof(targetCell1));
+      TargetCell2 = targetCell2 ?? throw new ArgumentNullException(nameof(targetCell2));
+   }
+
+   public LabyrinthCell ActualCell { get; }
+
+   public LabyrinthCell TargetCell1 { get; }
+
+   public LabyrinthCell TargetCell2 { get; }
+}
