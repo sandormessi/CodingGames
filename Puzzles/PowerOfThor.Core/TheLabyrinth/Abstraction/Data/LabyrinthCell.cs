1c301f132b86af973c1e5117ea3cfeea5cab3e6d
Sándor Ács <sandormessi@hotmail.com>
2023-06-20 20:22:24 +0200

    Implement IEqutable interface in LabyrinthCell and Position

--- Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/Data/LabyrinthCell.cs
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/Data/LabyrinthCell.cs
@@ -2,7 +2,7 @@
 
 using System;
 
-public class LabyrinthCell
+public class LabyrinthCell : IEquatable<LabyrinthCell>
 {
    public LabyrinthCell(LabyrinthCellType type, Position position)
    {
@@ -13,4 +13,42 @@ public class LabyrinthCell
    public Position Position { get; }
 
    public LabyrinthCellType Type { get; }
+
+   public bool Equals(LabyrinthCell? other)
+   {
+      if (other is null)
+      {
+         return false;
+      }
+
+      if (ReferenceEquals(this, other))
+      {
+         return true;
+      }
+
+      return Position.Equals(other.Position) && Type == other.Type;
+   }
+
+   public override bool Equals(object? obj)
+   {
+      if (obj is null)
+      {
+         return false;
+      }
+
+      if (ReferenceEquals(this, obj))
+      {
+         return true;
+      }
+
+      return obj.GetType() == GetType() && Equals((LabyrinthCell)obj);
+   }
+
+   public override int GetHashCode()
+   {
+      unchecked
+      {
+         return (Position.GetHashCode() * 397) ^ (int)Type;
+      }
+   }
 }
--- Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/Data/Position.cs
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/Data/Position.cs
@@ -2,7 +2,7 @@
 
 using System;
 
-public class Position
+public class Position : IEquatable<Position>
 {
    public Position(int x, int y)
    {
@@ -23,4 +23,47 @@ public class Position
    public int X { get; }
 
    public int Y { get; }
+
+   public bool Equals(Position? other)
+   {
+      if (other is null)
+      {
+         return false;
+      }
+
+      if (ReferenceEquals(this, other))
+      {
+         return true;
+      }
+
+      return X == other.X && Y == other.Y;
+   }
+
+   public override bool Equals(object? obj)
+   {
+      if (obj is null)
+      {
+         return false;
+      }
+
+      if (ReferenceEquals(this, obj))
+      {
+         return true;
+      }
+
+      if (obj.GetType() != this.GetType())
+      {
+         return false;
+      }
+
+      return Equals((Position)obj);
+   }
+
+   public override int GetHashCode()
+   {
+      unchecked
+      {
+         return (X * 397) ^ Y;
+      }
+   }
 }
