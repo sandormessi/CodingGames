2714c867a0b36080aaf381dac634f3140a3b3541
Sándor Ács <sandormessi@hotmail.com>
2023-06-20 20:12:31 +0200

    MovementManager empty implementation

---
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Implementation/Logic/MovementManager.cs
@@ -0,0 +1,25 @@
+﻿namespace TheLabyrinth.Abstraction.Logic;
+
+using System;
+
+using TheLabyrinth.Abstraction.Data;
+
+public class MovementManager : IMovementManager
+{
+   private readonly IOutputManager outputManager;
+
+   public MovementManager(IOutputManager outputManager)
+   {
+      this.outputManager = outputManager ?? throw new ArgumentNullException(nameof(outputManager));
+   }
+
+   public void Move(TargetCells targetCells)
+   {
+      if (targetCells == null)
+      {
+         throw new ArgumentNullException(nameof(targetCells));
+      }
+
+      throw new NotImplementedException();
+   }
+}
