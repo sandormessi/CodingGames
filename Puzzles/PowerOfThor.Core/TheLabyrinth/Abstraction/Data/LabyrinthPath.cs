0877bc0dcde3b142911a5784662dd8300d279670
Sándor Ács <sandormessi@hotmail.com>
2023-06-20 20:10:01 +0200

    Labyrinth path data class

---
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/Data/LabyrinthPath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheLabyrinth.Abstraction.Data;
+
+using System.Linq;
+
+public class LabyrinthPath
+{
+   public LabyrinthPath(IReadOnlyList<LabyrinthCell> cells)
+   {
+      Cells = cells ?? throw new ArgumentNullException(nameof(cells));
+
+      StartCell = cells[0];
+      TargetCell = cells.Last();
+      Distance = cells.Count;
+   }
+
+   public LabyrinthCell StartCell { get; }
+
+   public IReadOnlyList<LabyrinthCell> Cells { get; }
+
+   public LabyrinthCell TargetCell { get; }
+
+   public int Distance { get; }
+}
