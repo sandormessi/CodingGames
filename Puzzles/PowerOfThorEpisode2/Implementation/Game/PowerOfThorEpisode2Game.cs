f00cf30638431f50c788db70a390cb8fff5440e1
Sándor Ács <sandormessi@hotmail.com>
2023-06-20 21:43:11 +0200

    Comment out failing code

--- Puzzles/PowerOfThorEpisode2/Implementation/Game/PowerOfThorEpisode2Game.cs
+++ Puzzles/PowerOfThorEpisode2/Implementation/Game/PowerOfThorEpisode2Game.cs
@@ -10,31 +10,33 @@ namespace PowerOfThorEpisode2.Implementation.Game;
 
 using PowerOfThor.Core.Abstraction.Game;
 
+using TheLabyrinth;
+
 public class PowerOfThorEpisode2Game : IPowerOfThorGame
 {
     public void Execute()
     {
-        IInputReader inputReader = new InputReader();
-        IOutputManagerEpisode2 outputManager = new OutputManagerEpisode2();
-        IInitialGameDataReader<> initialGameDataReader = new InitialGameDataReader(inputReader);
-        IGameDataPerRoundReader gameDataPerRoundReader = new GameDataPerRoundReader(inputReader);
-        IActualMovementCalculator actualMovementCalculator = new ActualMovementCalculator();
+        //IInputReader inputReader = new InputReader();
+        //IOutputManagerEpisode2 outputManager = new OutputManagerEpisode2();
+        //IInitialGameDataReader<> initialGameDataReader = new InitialGameDataReader(inputReader);
+        //IGameDataPerRoundReader gameDataPerRoundReader = new GameDataPerRoundReader(inputReader);
+        //IActualMovementCalculator actualMovementCalculator = new ActualMovementCalculator();
 
-        IGameDataReader gameDataReader = new GameDataReader(initialGameDataReader, gameDataPerRoundReader);
-        IGameLogicManager gameLogicManager = new GameLogicManager(outputManager, actualMovementCalculator);
+        //IGameDataReader gameDataReader = new GameDataReader(initialGameDataReader, gameDataPerRoundReader);
+        //IGameLogicManager gameLogicManager = new GameLogicManager(outputManager, actualMovementCalculator);
 
-        int round = 1;
+        //int round = 1;
 
-        GameData gameData = gameDataReader.ReadGameData(round);
+        //GameData gameData = gameDataReader.ReadGameData(round);
 
-        var lastPosition = gameData.InitialGameData.ThorPosition;
-        while (true)
-        {
-            var actualMovement = gameLogicManager.Execute(gameData, lastPosition);
-            lastPosition = actualMovement.ActualPosition;
-            round++;
+        //var lastPosition = gameData.InitialGameData.ThorPosition;
+        //while (true)
+        //{
+        //    var actualMovement = gameLogicManager.Execute(gameData, lastPosition);
+        //    lastPosition = actualMovement.ActualPosition;
+        //    round++;
 
-            gameData = gameDataReader.ReadGameData(round);
-        }
+        //    gameData = gameDataReader.ReadGameData(round);
+        //}
     }
 }
