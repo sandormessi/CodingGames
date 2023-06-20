8bd4852d6dcae97e1b1f778168c5d9640d1375a6
Sándor Ács <sandormessi@hotmail.com>
2023-06-20 20:37:00 +0200

    TargetPath data class

---
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/Data/TargetPath.cs
@@ -0,0 +1,19 @@
+﻿namespace TheLabyrinth.Abstraction.Data;
+
+using System;
+
+public class TargetPath
+{
+   public TargetPath(LabyrinthPath path, int actualPosition)
+   {
+      Path = path ?? throw new ArgumentNullException(nameof(path));
+      ActualPosition = actualPosition;
+      TargetReached = ActualPosition == path.Distance;
+   }
+
+   public LabyrinthPath Path { get; }
+
+   public int ActualPosition { get; }
+
+   public bool TargetReached { get; }
+}
