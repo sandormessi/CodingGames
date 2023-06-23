f3daa711af15343f5eb7433124096eaf6cbe8ee5
Sándor Ács <sandormessi@hotmail.com>
2023-06-23 21:15:46 +0200

    IMovementAnalyzer

---
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/Data/MovementAnalysisResult.cs
@@ -0,0 +1,21 @@
+﻿namespace TheLabyrinth.Abstraction.Data;
+
+using System;
+
+public class MovementAnalysisResult
+{
+   public MovementAnalysisResult(bool canMove, int cellsToExplore)
+   {
+      if (cellsToExplore < 0)
+      {
+         throw new ArgumentOutOfRangeException(nameof(cellsToExplore));
+      }
+
+      CanMove = canMove;
+      CellsToExplore = cellsToExplore;
+   }
+
+   public bool CanMove { get; }
+
+   public int CellsToExplore { get; }
+}
---
+++ Puzzles/PowerOfThor.Core/TheLabyrinth/Abstraction/IMovementAnalyzer.cs
@@ -0,0 +1,10 @@
+﻿namespace TheLabyrinth.Abstraction;
+
+using System.Collections.Generic;
+
+using TheLabyrinth.Abstraction.Data;
+
+public interface IMovementAnalyzer
+{
+   IDictionary<Direction, MovementAnalysisResult> Analyze(ExtendedLabyrinth labyrinth, ExtendedLabyrinthCell actualPositionCell, InitialGameInfo initialGameInfo);
+}
